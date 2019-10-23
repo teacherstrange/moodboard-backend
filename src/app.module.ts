924676a941b122f73dee9324d5884e51e6893f17
musta_fa <mustakadem@gmail.com>
2019-10-23 14:01:13 +0200

    add ORM config and GraphQl module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot(),
+    GraphQLModule.forRoot({
+      autoSchemaFile: 'schema.gpl',
+    }),
+  ],
+  controllers: [ AppController ],
+  providers: [ AppService ],
 })
 export class AppModule {}
