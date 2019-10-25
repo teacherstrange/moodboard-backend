8e7c6bd2d3c21599be9148ebc7a652b1cfde66e1
musta_fa <mustakadem@gmail.com>
2019-10-25 14:16:09 +0200

    feat() add custom validation

--- src/modules/users/dto/loginUser.dto.ts
+++ src/modules/users/dto/loginUser.dto.ts
@@ -1,14 +1,22 @@
 import { Field, ObjectType} from 'type-graphql';
-import { IsEmail, IsString } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 @ObjectType()
 export class LoginUserDto {
 
   @Field()
-  @IsEmail()
+  @IsEmail(undefined, {
+    message: 'email must be an email address.',
+  })
+  @IsNotEmpty({
+    message: 'email field is required.',
+  })
   readonly email: string;
 
   @Field()
   @IsString()
+  @IsNotEmpty({
+    message: 'password field is required.',
+  })
   readonly password: string;
 }
